c38ad08da16353853038b8cfcf6ae7ff7c1b2fad
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-01-28 17:52:21 -0500

    updating the extension of .jsx to .js

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -17,7 +17,6 @@ export const SearchPage = () => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
-
         <p className="font-light">Search a hero</p>
       </div>
       <div className="mt-5 bg-yellow-100 p-5 rounded-lg text-yellow-600 border border-yellow-300/40 flex items-center gap-3">
