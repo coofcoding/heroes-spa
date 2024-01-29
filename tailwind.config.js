8f7187380aa1271e87560114193bcc30175e1b67
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-01-29 17:13:33 -0500

    fixing the flowbite installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ const plugin = require('tailwindcss/plugin');
 
 export default {
     content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/flowbite/**/*.js"
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/flowbite/**/*.js',
+        'node_modules/flowbite-react/lib/esm/**/*.js'
     ],
     theme: {
         extend: {},
